370d6f3ef875a530626ca4fbd8378fd75d9a69bd
xing zhe <xingzheke@gmail.com>
2017-04-21 13:08:34 +0800

    重新提交

--- mkerckumao.sh
+++ mkerckumao.sh
@@ -1,11 +1,11 @@
 #/bin/sh
 
-echo "{\"cmd-param\":\"-e -k 'MIIEpQIBAAKCAQEA5ANvIDDY1a+6bEb8LjW6e9HdcFIung' -b BYE --disable-compile\",\"COCOS_CONSOLE_ROOT\":\""$COCOS_CONSOLE_ROOT"\"}">jsonmaker.config
+echo "{\"cmd-param\":\"-e -k 'BKDLRpQIBddvcCAQEA5ANvIDDY3a+4bEb8LjW6e9HERDD' -b DIANWAN --disable-compile\",\"COCOS_CONSOLE_ROOT\":\""$COCOS_CONSOLE_ROOT"\"}">jsonmaker.config
 
 rm -rf erc
 mkdir erc
 cp -r src erc/src
-cocos luacompile -s erc/src -d erc/src -e -k "MIIEpQIBAAKCAQEA5ANvIDDY1a+6bEb8LjW6e9HdcFIung" -b BYE --disable-compile
+cocos luacompile -s erc/src -d erc/src -e -k "BKDLRpQIBddvcCAQEA5ANvIDDY3a+4bEb8LjW6e9HERDD" -b DIANWAN --disable-compile
 cd erc
 find . -name "*.lua" -exec rm -rf {} \;
 rm -rf ../res/src.zip
